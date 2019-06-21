7b47d9f2bb094017dbbd869334c600dd1e0b6b41
Carston Henderson <carstonhenderson@icloud.com>
2019-06-21 11:20:44 -0600

    Update gatsby-node.js
    - add slug field to pokemon
    - create page for each pokemon

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === 'Pokemon') {
+    const slug = `/${node.name.toLowerCase()}`
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allPokemon {
+        nodes {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allPokemon.nodes.forEach(pokemon => {
+      createPage({
+        path: pokemon.fields.slug,
+        component: path.resolve(`./src/templates/pokemon.jsx`),
+        context: {
+          slug: pokemon.fields.slug,
+        },
+      })
+    })
+  })
+}
