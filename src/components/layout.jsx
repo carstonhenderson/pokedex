da56f227fb6603fbfc07e1a5010ceeda9ab9ac5f
Carston Henderson <carstonhenderson@icloud.com>
2019-06-26 10:18:48 -0600

    Fix background color issue when the page is larger than the screen

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './layout.css'
 
 const Layout = ({ children }) => (
-  <div className="bg-gray-900 text-white h-screen">
+  <div className="bg-gray-900 text-white min-h-screen">
     <main className="container mx-auto overflow-auto p-4 bg-gray-900 text-white">
       {children}
     </main>
