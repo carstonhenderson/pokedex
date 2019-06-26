e37957f6aa33bde0ea2897adf6ae65b1054da599
Carston Henderson <carstonhenderson@icloud.com>
2019-06-26 10:14:06 -0600

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,39 +1,12 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react'
-import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
-
-import Header from './header'
 import './layout.css'
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <div className="bg-gray-900 text-white">
-      <Header siteTitle={data.site.siteMetadata.title} />
-
-      <main className="container mx-auto overflow-auto p-4">{children}</main>
-    </div>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+const Layout = ({ children }) => (
+  <div className="bg-gray-900 text-white h-screen">
+    <main className="container mx-auto overflow-auto p-4 bg-gray-900 text-white">
+      {children}
+    </main>
+  </div>
+)
 
 export default Layout
