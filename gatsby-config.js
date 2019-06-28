ec8a3af6d246307f854cb0cb32ce56efb70615b8
Carston Henderson <carstonhenderson@icloud.com>
2019-06-28 11:48:19 -0600

    Fix iOS PWA display issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
