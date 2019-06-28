cf73f8597209ccf1037fa12b19644c469cd1f096
Carston Henderson <carstonhenderson@icloud.com>
2019-06-28 10:57:11 -0600

    Reset pagination when search filter changes

--- src/components/filterablePokemonTable.jsx
+++ src/components/filterablePokemonTable.jsx
@@ -4,11 +4,28 @@ import PaginatedPokemonTable from './paginatedPokemonTable'
 
 const FilterablePokemonTable = ({ pokemon }) => {
   const [filter, setFilter] = useState('')
+  const [page, setPage] = useState(1)
+
+  const start = (page - 1) * 10
+  const end = page * 10
 
   return (
     <div>
-      <PokemonSearchFilter value={filter} onChange={e => setFilter(e.target.value)} />
-      <PaginatedPokemonTable filter={filter} pokemon={pokemon} />
+      <PokemonSearchFilter
+        value={filter}
+        onChange={e => {
+          setFilter(e.target.value)
+          setPage(1)
+        }}
+      />
+      <PaginatedPokemonTable
+        filter={filter}
+        pokemon={pokemon}
+        start={start}
+        end={end}
+        handlePrevious={() => setPage(page - 1)}
+        handleNext={() => setPage(page + 1)}
+      />
     </div>
   )
 }
--- src/components/paginatedPokemonTable.jsx
+++ src/components/paginatedPokemonTable.jsx
@@ -1,45 +1,46 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PokemonTable from './pokemonTable'
 import PokemonPagination from './pokemonPagination'
 
-const PaginatedPokemonTable = ({ filter, pokemon }) => {
-  const [page, setPage] = useState(1)
+const PaginatedPokemonTable = ({
+  filter,
+  pokemon,
+  start,
+  end,
+  handlePrevious,
+  handleNext,
+}) => {
+  const filteredPokemon = pokemon.filter(pokemon => {
+    const nameMatches = pokemon.name
+      .toLowerCase()
+      .includes(filter.toLowerCase())
 
-  const start = (page - 1) * 10
-  const end = page * 10
+    const typeMatches =
+      pokemon.type.filter(type => {
+        return type.toLowerCase().includes(filter.toLowerCase())
+      }).length > 0
 
-  const filteredPokemon = pokemon
-    .filter(pokemon => {
-      const nameMatches = pokemon.name
-        .toLowerCase()
-        .includes(filter.toLowerCase())
+    return nameMatches || typeMatches
+  })
 
-      const typeMatches =
-        pokemon.type.filter(type => {
-          return type.toLowerCase().includes(filter.toLowerCase())
-        }).length > 0
-
-      return nameMatches || typeMatches
-    })
-
-    if (filteredPokemon.length > 0) {
-      return (
-        <>
-          <PokemonTable pokemon={filteredPokemon.slice(start, end)} />
-          <PokemonPagination
-            handlePrevious={() => setPage(page - 1)}
-            handleNext={() => setPage(page + 1)}
-            start={start + 1}
-            end={end}
-            total={filteredPokemon.length}
-          />
-        </>
-      )
-    } else {
-      return (
-        <div className="text-gray-500 text-center my-16">No results found</div>
-      )
-    }
+  if (filteredPokemon.length > 0) {
+    return (
+      <>
+        <PokemonTable pokemon={filteredPokemon.slice(start, end)} />
+        <PokemonPagination
+          handlePrevious={handlePrevious}
+          handleNext={handleNext}
+          start={start + 1}
+          end={end}
+          total={filteredPokemon.length}
+        />
+      </>
+    )
+  } else {
+    return (
+      <div className="text-gray-500 text-center my-16">No results found</div>
+    )
+  }
 }
 
 export default PaginatedPokemonTable
