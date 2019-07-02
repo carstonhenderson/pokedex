437c5ee0726753b8292c6bd10e09e73e5cda914f
Carston Henderson <carstonhenderson@icloud.com>
2019-07-02 11:25:49 -0600

    Update icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#1a202c`,
         theme_color: `#1a202c`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/pokedex-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
